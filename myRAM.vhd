ed1e48844fd13b9b8d4285bc5dc05f9facebf79c
NoamElbaum <noam272000@gmail.com>
2020-04-11 15:25:18 +0300

    Update myRAM.vhd

--- myRAM.vhd
+++ myRAM.vhd
@@ -4,17 +4,17 @@ use ieee.std_logic_unsigned.all;
 
 entity myRam is
 
-generic(NumOfBytes :integer := 8);
+generic(NumOfAddressBits :integer := 8);
 
 port( DataIn					:in std_logic_vector(7 downto 0);
-		address_W, address_R :in std_logic_vector(NumOfBytes-1 downto 0);
+		address_W, address_R :in std_logic_vector(NumOfAddressBits-1 downto 0);
 		DataOut					:out std_logic_vector(7 downto 0);
 		Clock, W_R 			   :in std_logic);
 end entity;
 
 architecture arch_myRAM of myRAM is
 
-type MEM is array (NumOfBytes-1 downto 0) of std_logic_vector(7 downto 0);
+type MEM is array (NumOfAddressBits-1 downto 0) of std_logic_vector(7 downto 0);
 signal Memory : MEM; 
 
 begin
